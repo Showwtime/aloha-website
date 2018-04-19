e7b82c7c989913d2e1424dd2d9aee034e983d974
Showwtime <jason.f.murphy@gmail.com>
2018-04-19 15:44:05 -0700

    Carousel added, submit js added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,48 @@
-console.log("Its WORKING");
+$(document).ready(function() { //<--- document ready lets page load before applying jQuery
+
+
+
+// carousel
+
+$('.main-carousel').flickity({
+    // options
+    cellAlign: 'left',
+    contain: true
+  });
+
+
+
+
+
+
+
+
+// subscribe email validation.
+$(".email").on("submit", function(event) {
+    event.preventDefault();
+    if ($("#my-text-input").val() == "") {
+    alert("You missed the field.");
+    } else {
+    alert("Thanks for filling the field!");
+}
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}); // end of doc ready
